02548345cb45789a5e2da8a1907e45a6841f9bea
dtutila <daniel@dtutila.com>
2024-06-27 13:58:12 -0600

    - added dotenv - added parser

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('hello')
+from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.llms import OpenAI
+from langchain.chains import LLMChain
+import argparse
+
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+args = parser.parse_args()
+
+
+print("hello")
