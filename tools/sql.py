3ded77d7152003f111047dbe21e2714fb38c8da6
dtutila <daniel@dtutila.com>
2024-07-09 23:11:26 -0600

    added run query function

---
+++ tools/sql.py
@@ -0,0 +1,17 @@
+import sqlite3
+from langchain.tools import Tool
+
+conn = sqlite3.connect('agent.db')
+
+
+def run_sqlite_query(query):
+    c = conn.cursor()
+    c.execute(query)
+    return c.fetchall()
+
+
+run_query_tool = Tool.from_function(
+    name='run_sqlite_query',
+    description="run sql query",
+    func=run_sqlite_query,
+)
